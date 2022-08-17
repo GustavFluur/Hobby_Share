04998adaaac1cf52e256d1828a0da9fdaa872795
GustavFluur <gustavfluur@hotmail.com>
2022-08-17 12:59:40 +0000

    Add new content for testing within views.py & urls.py files

--- Share_Hobby/views.py
+++ Share_Hobby/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from Share_Hobby.views import say_hello
 
 # Create your views here.
+def say_hello(request):
+    return HttpResponse("Hello!")
