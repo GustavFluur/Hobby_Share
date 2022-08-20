68fc727d4f9da416cdf7fd70ec7e64dd69a0c9c5
GustavFluur <gustavfluur@hotmail.com>
2022-08-20 09:43:02 +0000

    Add new installations into project and migration has been made

--- Hobby_Share/urls.py
+++ Hobby_Share/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Share_Hobby.views import say_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Share_Hobby/views.py
+++ Share_Hobby/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from Share_Hobby.views import say_hello
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello!")
+    return HttpResponse("Hello Gustav, you can do this, just keep your head straight!")
