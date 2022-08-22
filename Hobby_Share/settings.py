66709ccaa20c2a68a31c95978b840d9756b22736
GustavFluur <gustavfluur@hotmail.com>
2022-08-22 08:04:53 +0000

    Add Procfile into workspace with details necessary to make it operational

--- Hobby_Share/settings.py
+++ Hobby_Share/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Hobby_Share.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'Hobby_Share.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,6 +76,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'Hobby_Share.wsgi.application'
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Hobby_Share.wsgi
