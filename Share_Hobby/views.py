a545c47d9a409df63ceed93ad33ec730efb21eb2
GustavFluur <gustavfluur@hotmail.com>
2022-08-29 19:31:20 +0000

    Added a new attribute to make it easy for all user to commment without any approvements

--- Share_Hobby/views.py
+++ Share_Hobby/views.py
@@ -17,7 +17,7 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
+        comments = post.comments.filter(approved=False).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
