aba8847003de97637324ba77d2ebf25270e67ccb
GustavFluur <gustavfluur@hotmail.com>
2022-09-26 16:07:48 +0000

    Add and change of flush accordion for about page

--- Hobby_Share/urls.py
+++ Hobby_Share/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Share_Hobby.urls'), name='Share_Hobby_urls'),
