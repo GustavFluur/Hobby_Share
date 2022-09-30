5f44030c6f77241535778e55ae411c3041c219fe
GustavFluur <gustavfluur@hotmail.com>
2022-09-30 09:14:56 +0000

    Add specific user feauture that only one can edit or delete their own posts

--- Share_Hobby/models.py
+++ Share_Hobby/models.py
@@ -7,8 +7,8 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=225, unique=True)
+    slug = models.SlugField(max_length=225, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
