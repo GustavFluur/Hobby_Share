70e59a7da8b0e0e1962b493b337764a4e8a30cd2
GustavFluur <gustavfluur@hotmail.com>
2022-11-03 10:18:41 +0000

    Add change in DEBUG settings to a new variable in settings and env files

--- Hobby_Share/settings.py
+++ Hobby_Share/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
+DEBUG = 'DEV' in os.environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -28,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ["sharehobbies.herokuapp.com", "localhost"]
 
